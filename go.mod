a0da9fd78de9239bf3adf1320c4c99c9a01fc63c
vitams <vmpartner@gmail.com>
2018-11-16 03:05:26 +0500

    Init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/vmpartner/go-pgdb
+
+require (
+	github.com/jinzhu/gorm v1.9.1 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/lib/pq v1.0.0 // indirect
+)
---
+++ pgdb.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"time"
+)
+
+var User string
+var Pass string
+var Host string
+var Port string
+var Name string
+var Ping int
+var Debug bool
+
+var DB *gorm.DB
+var lastPing time.Time
+
+func New() *gorm.DB {
+
+	if Ping <= 0 {
+		Ping = 1
+	}
+
+	if DB == nil {
+		Connect()
+		return New()
+	}
+	if time.Now().After(lastPing.Add(time.Duration(Ping) * time.Minute)) {
+		lastPing = time.Now()
+		err := DB.DB().Ping()
+		if err != nil {
+			DB.Close()
+			Connect()
+			return New()
+		}
+	}
+
+	return DB
+}
+
+func Close() {
+	DB.Close()
+}
+
+func Connect() {
+	dbLink := GetLInk()
+	var err error
+	DB, err = gorm.Open("postgres", dbLink)
+	DB.LogMode(Debug)
+	if err != nil {
+		panic(err.Error())
+	}
+	DB.DB().SetMaxIdleConns(100)
+	DB.DB().SetMaxOpenConns(1000)
+}
+
+func GetLInk() string {
+	dbLink := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", User, Pass, Host, Port, Name)
+
+	return dbLink
+}
